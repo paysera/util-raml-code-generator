5e04ff684d50d032d14ac35e61ffbf304f3f22ee
Dilanas Sabaliauskas <d.sabaliauskas@paysera.com>
2020-05-04 15:42:28 +0300

    Rename variable and change question

--- src/Paysera/Bundle/ClientReleaseBundle/Service/ReleaseStep/DeleteFilesStep.php
+++ src/Paysera/Bundle/ClientReleaseBundle/Service/ReleaseStep/DeleteFilesStep.php
@@ -3,7 +3,6 @@
 namespace Paysera\Bundle\ClientReleaseBundle\Service\ReleaseStep;
 
 use Paysera\Bundle\ClientReleaseBundle\Entity\ReleaseStepData;
-use Paysera\Bundle\ClientReleaseBundle\Exception\ReleaseCycleException;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -27,7 +26,7 @@ class DeleteFilesStep implements ReleaseStepInterface
             ->getIterator()
         ;
 
-        $deletedFiles = [];
+        $filesToDelete = [];
         foreach ($iterator as $sourceFile) {
             $generatedFilename = str_replace(
                 $releaseStepData->getSourceDir(),
@@ -35,20 +34,26 @@ class DeleteFilesStep implements ReleaseStepInterface
                 $sourceFile->getPathname()
             );
             if (!($this->filesystem->exists($generatedFilename))) {
-                $deletedFiles[] = $sourceFile;
+                $filesToDelete[] = $sourceFile;
             }
         }
-        if (count($deletedFiles) === 0) {
+        $output->writeln('');
+        if (count($filesToDelete) === 0) {
             $output->writeln('<info>There are no old source files to be deleted.</info>');
+
             return;
         }
 
+        $output->writeln('<notice>Files to be deleted:</notice>');
+        foreach ($filesToDelete as $fileToDelete) {
+            $output->writeln($fileToDelete->getRelativePathname());
+        }
+
         $questionHelper = new QuestionHelper();
-        $output->writeln('');
         $question = new ConfirmationQuestion(
             sprintf(
-                '<info>Do you want to delete old source files (%d found)? (Y|n) </info>',
-                count($deletedFiles)
+                '<info>Do you want to delete these old source files (%d found)? (Y|n) </info>',
+                count($filesToDelete)
             )
         );
         if (!$questionHelper->ask($input, $output, $question)) {
@@ -56,19 +61,6 @@ class DeleteFilesStep implements ReleaseStepInterface
         }
         $output->writeln('');
 
-
-        $output->writeln('<notice>Files to be deleted:</notice>');
-        foreach ($deletedFiles as $deletedFile) {
-            $output->writeln($deletedFile->getRelativePathname());
-        }
-
-        $output->writeln('');
-        $question = new ConfirmationQuestion('<info>Do you accept? (Y|n) </info>');
-        if (!$questionHelper->ask($input, $output, $question)) {
-            throw new ReleaseCycleException('User did not accept file deletion.');
-        }
-        $output->writeln('');
-
-        $this->filesystem->remove($deletedFiles);
+        $this->filesystem->remove($filesToDelete);
     }
 }
