c39aa2e2f1580ab01b8edd601b04ee7e40d18f8f
Shaharia Azam <s.azam@paysera.com>
2021-02-12 16:10:37 +0600

    Phar binary not found issue fixed during release:clients command. It now find phar binary from local and global path

--- src/Paysera/Bundle/ClientReleaseBundle/Service/ReleaseStep/GeneratePhpClientStep.php
+++ src/Paysera/Bundle/ClientReleaseBundle/Service/ReleaseStep/GeneratePhpClientStep.php
@@ -8,10 +8,14 @@ use Paysera\Bundle\ClientReleaseBundle\Entity\ReleaseStepData;
 use Paysera\Bundle\ClientReleaseBundle\Exception\ReleaseCycleException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\RuntimeException;
+use Symfony\Component\Process\ExecutableFinder;
 use Symfony\Component\Process\Process;
 
 class GeneratePhpClientStep implements ReleaseStepInterface
 {
+    const PHAR_FILE_NAME = 'raml-code-generator';
+
     public function processStep(ReleaseStepData $releaseStepData, InputInterface $input, OutputInterface $output)
     {
         /** @var PhpClientDefinition $clientDefinition */
@@ -19,7 +23,7 @@ class GeneratePhpClientStep implements ReleaseStepInterface
         $releaseStepData->setGeneratedDir($releaseStepData->getTempDir() . '/generated');
 
         $args = [
-            'raml-code-generator',
+            $this->findPharBinaryPath(),
             'php-generator:rest-client',
             'raml_file' => $releaseStepData->getApiConfig()->getRamlFile(),
             'output_dir' => $releaseStepData->getGeneratedDir(),
@@ -44,5 +48,27 @@ class GeneratePhpClientStep implements ReleaseStepInterface
             '<info>*</info> Generated PHP code for Api "%s"',
             $releaseStepData->getApiConfig()->getApiName()
         ));
+    }
+
+    private function findPharBinaryPath()
+    {
+        $localPath = $this->getCurrentDirectory() . '/bin/' . self::PHAR_FILE_NAME;
+        if (file_exists($localPath)) {
+            return $localPath;
+        }
+
+        $globalPath = (new ExecutableFinder())
+            ->find('raml-code-generator');
+
+        if ($globalPath === null) {
+            throw new RuntimeException('Failed to find phar binary');
+        }
+
+        return $globalPath;
+    }
+
+    private function getCurrentDirectory()
+    {
+        return str_replace('/bin', '', getcwd());
     }
 }
