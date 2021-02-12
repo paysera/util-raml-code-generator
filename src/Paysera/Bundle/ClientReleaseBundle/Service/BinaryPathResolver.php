8b9eba881d2a8c4421b8f7d2a0b4cf5608700e2e
Shaharia Azam <s.azam@paysera.com>
2021-02-12 18:21:15 +0600

    BinaryPathResolver.php service created to discover binary phar path

---
+++ src/Paysera/Bundle/ClientReleaseBundle/Service/BinaryPathResolver.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Paysera\Bundle\ClientReleaseBundle\Service;
+
+use Symfony\Component\Process\Exception\RuntimeException;
+use Symfony\Component\Process\ExecutableFinder;
+
+class BinaryPathResolver
+{
+    private $additionalLookupDirectories;
+
+    public function __construct(array $additionalLookupDirectories)
+    {
+        $this->additionalLookupDirectories = $additionalLookupDirectories;
+    }
+
+    public function getPath(array $possibleBinaryNames): string
+    {
+        foreach ($possibleBinaryNames as $binary) {
+            $path = (new ExecutableFinder())
+                ->find($binary, null, $this->additionalLookupDirectories);
+
+            if ($path !== null) {
+                return $path;
+            }
+        }
+
+        throw new RuntimeException(
+            sprintf(
+                'Failed to find any of %s phar binary',
+                implode(',', $possibleBinaryNames)
+            )
+        );
+    }
+}
--- src/Paysera/Bundle/ClientReleaseBundle/Service/ReleaseStep/GeneratePhpClientStep.php
+++ src/Paysera/Bundle/ClientReleaseBundle/Service/ReleaseStep/GeneratePhpClientStep.php
@@ -6,15 +6,21 @@ namespace Paysera\Bundle\ClientReleaseBundle\Service\ReleaseStep;
 use Paysera\Bundle\ClientReleaseBundle\Entity\PhpClientDefinition;
 use Paysera\Bundle\ClientReleaseBundle\Entity\ReleaseStepData;
 use Paysera\Bundle\ClientReleaseBundle\Exception\ReleaseCycleException;
+use Paysera\Bundle\ClientReleaseBundle\Service\BinaryPathResolver;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Exception\RuntimeException;
-use Symfony\Component\Process\ExecutableFinder;
 use Symfony\Component\Process\Process;
 
 class GeneratePhpClientStep implements ReleaseStepInterface
 {
     const PHAR_FILE_NAME = 'raml-code-generator';
+
+    private $binaryPathResolver;
+
+    public function __construct(BinaryPathResolver $binaryPathResolver)
+    {
+        $this->binaryPathResolver = $binaryPathResolver;
+    }
 
     public function processStep(ReleaseStepData $releaseStepData, InputInterface $input, OutputInterface $output)
     {
@@ -23,7 +29,12 @@ class GeneratePhpClientStep implements ReleaseStepInterface
         $releaseStepData->setGeneratedDir($releaseStepData->getTempDir() . '/generated');
 
         $args = [
-            $this->findPharBinaryPath(),
+            $this->binaryPathResolver->getPath(
+                [
+                    self::PHAR_FILE_NAME,
+                    self::PHAR_FILE_NAME . '.phar',
+                ]
+            ),
             'php-generator:rest-client',
             'raml_file' => $releaseStepData->getApiConfig()->getRamlFile(),
             'output_dir' => $releaseStepData->getGeneratedDir(),
@@ -48,27 +59,5 @@ class GeneratePhpClientStep implements ReleaseStepInterface
             '<info>*</info> Generated PHP code for Api "%s"',
             $releaseStepData->getApiConfig()->getApiName()
         ));
-    }
-
-    private function findPharBinaryPath()
-    {
-        $localPath = $this->getCurrentDirectory() . '/bin/' . self::PHAR_FILE_NAME;
-        if (file_exists($localPath)) {
-            return $localPath;
-        }
-
-        $globalPath = (new ExecutableFinder())
-            ->find('raml-code-generator');
-
-        if ($globalPath === null) {
-            throw new RuntimeException('Failed to find phar binary');
-        }
-
-        return $globalPath;
-    }
-
-    private function getCurrentDirectory()
-    {
-        return str_replace('/bin', '', getcwd());
     }
 }
