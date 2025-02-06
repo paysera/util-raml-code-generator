92a02dd36990a1fd3b3e26f9f1497b999ca4d264
Aurimas Pelanis <a.pelanis@paysera.lt>
2025-02-06 07:04:35 +0200

    ci: migrate from Travis to GitHub Actions

--- src/Paysera/Component/StringHelper.php
+++ src/Paysera/Component/StringHelper.php
@@ -4,6 +4,9 @@ namespace Paysera\Component;
 
 use Doctrine\Common\Inflector\Inflector;
 
+/**
+ * Helper class for string manipulations like case conversions and pluralization
+ */
 class StringHelper
 {
     public static function kebabCase($string)
